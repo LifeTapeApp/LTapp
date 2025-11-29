636a25e6bd5efebc60acdefa5157460e7684807a
LifeTapeApp <lifetapeapp@gmail.com>
2025-11-29 02:13:03 -0500

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -1,9 +1,14 @@
+// app.config.js
 export default ({ config }) => {
   return {
     ...config,
-    extra: {
-      supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
-      supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
+    expo: {
+      ...(config.expo || {}),
+      extra: {
+        supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL || process.env.NEXT_PUBLIC_SUPABASE_URL,
+        supabaseAnonKey:
+          process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+      },
     },
   };
 };
