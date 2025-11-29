e6e7f4e9383192541a0a1fdfbfc39739e17040e9
LifeTapeApp <lifetapeapp@gmail.com>
2025-11-29 00:21:31 -0500

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -3,4 +3,8 @@ import Constants from 'expo-constants';
 
 const { supabaseUrl, supabaseAnonKey } = Constants.expoConfig.extra;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+export const supabase = createClient(
+  process.env.EXPO_PUBLIC_SUPABASE_URL,
+  process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY
+);
