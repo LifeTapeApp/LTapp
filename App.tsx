d710c771b0849fcf453961043ce750e5f4b61cf3
LifeTapeApp <lifetapeapp@gmail.com>
2025-11-29 02:28:08 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -177,41 +177,6 @@ const useUserStore = create<UserState>()(
 );
 
 // ----------------------------
-// PIN STORE
-// ----------------------------
-type PINState = {
-  pin: string | null;
-  isAppUnlocked: boolean;
-  lastBackgroundTime: number | null;
-
-  setPIN: (p: string) => void;
-  hasAppPIN: () => boolean;
-  unlockApp: () => void;
-  lockApp: () => void;
-  setLastBackgroundTime: (t: number) => void;
-};
-
-const usePINStore = create<PINState>()(
-  persist(
-    (set, get) => ({
-      pin: null,
-      isAppUnlocked: false,
-      lastBackgroundTime: null,
-
-      setPIN: (p) => set({ pin: p }),
-      hasAppPIN: () => Boolean(get().pin),
-      unlockApp: () => set({ isAppUnlocked: true }),
-      lockApp: () => set({ isAppUnlocked: false }),
-      setLastBackgroundTime: (t) => set({ lastBackgroundTime: t }),
-    }),
-    {
-      name: "life-tape-pin-storage",
-      storage: supabaseStorage,
-    }
-  )
-);
-
-// ----------------------------
 // APP STATE STORE (entries, recordings, etc.)
 // ----------------------------
 type AppState = {
