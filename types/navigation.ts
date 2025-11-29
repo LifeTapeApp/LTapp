1c2ee10fe916a43f62599d1b1ab203984ec247f6
LifeTapeApp <lifetapeapp@gmail.com>
2025-11-29 02:59:33 -0500

    Create navigation.ts

---
+++ types/navigation.ts
@@ -0,0 +1,17 @@
+export type RootStackParamList = {
+  Splash: undefined;
+  Onboarding: undefined;
+  AccountCreation: undefined;
+  TimelineBuilder: undefined;
+  PINSetup: undefined;
+  Main: undefined;
+  DarkSide: undefined;
+  Settings: undefined;
+  TitlePreference: undefined;
+  ChangePassword: undefined;
+  PrivacySecurity: undefined;
+  Contact: undefined;
+  SendInvite: undefined;
+  DownloadStory: undefined;
+  EntryDetail: { entryId: string };
+};
